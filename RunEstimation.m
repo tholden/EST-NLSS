b2201d60f272016f70971726e96ae0f91f240890
Tom Holden <thomas.holden@gmail.com>
2017-02-16 17:39:31 +0000

    set any unset options

--- RunEstimation.m
+++ RunEstimation.m
@@ -3,6 +3,8 @@ function [ Parameters, PersistentState ] = RunEstimation( Parameters, Options, P
     NumParameters = size( Parameters, 1 );
     NumObservables = size( Options.Data, 2 );
     
+    Options = SetDefaultOptions( Options );
+    
     InputParameters = [ Parameters; bsxfun( @plus, log( Options.InitialMEStd ), zeros( NumObservables, 1 ) ) ];
         
     EstimatedNu = ~Options.NoTLikelihood && ~Options.DynamicNu;
