24752a184a47cad016fcb6c295e7787e2658c491
Tom Holden <thomas.holden@gmail.com>
2017-02-17 13:51:39 +0000

    more skeleton code

--- RunSmoothing.m
+++ RunSmoothing.m
@@ -3,6 +3,18 @@ function [ Parameters, PersistentState ] = RunSmoothing( Parameters, Options, Pe
     CorePath = [ fileparts( which( 'RunSmoothing' ) ) '/Core/' ];
     addpath( CorePath );
 
+    NumParameters = size( Parameters, 1 );
+    NumObservables = size( Options.Data, 1 );
+    
+    Options = SetDefaultOptions( Options );
+    
+    InputParameters = [ Parameters; bsxfun( @plus, log( Options.InitialMEStd ), zeros( NumObservables, 1 ) ) ];
+        
+    EstimatedNu = ~Options.NoTLikelihood && ~Options.DynamicNu;
+    if EstimatedNu
+        InputParameters( end + 1 ) = log( Options.InitialNu );
+    end    
+    
     rmpath( CorePath );
 
 end
