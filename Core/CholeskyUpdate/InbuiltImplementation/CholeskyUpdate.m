a02b81130aae495fb8663076480de8ab5bb0d567
tholden <thomas.holden@gmail.com>
2017-03-02 18:30:48 +0000

    alternative implementation

---
+++ Core/CholeskyUpdate/InbuiltImplementation/CholeskyUpdate.m
@@ -0,0 +1,13 @@
+function [ R, p ] = CholeskyUpdate( R, x, SignString )
+    if nargin < 3
+        SignString = '+';
+    end
+    if nargout < 2
+        R = cholupdate( R, x, SignString );
+    else
+        [ R, p ] = cholupdate( R, x, SignString );
+    end
+end
+
+% while true; R = randn( 10 ); A = R' * R; R = chol( A ); x = randn( 10, 1 ); [ R1, p ] = cholupdate( R, x, '+' ); if p == 0; disp( max( max( abs( R1' * R1 - A - x * x' ) ) ) ); end; end;
+% while true; R = randn( 10 ); A = R' * R; R = chol( A ); x = randn( 10, 1 ); [ R1, p ] = cholupdate( R, x, '-' ); if p == 0; disp( max( max( abs( R1' * R1 - A + x * x' ) ) ) ); end; end;
