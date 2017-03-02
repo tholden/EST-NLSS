cd9728234bc396f158a0174366c82886f2e55157
tholden <thomas.holden@gmail.com>
2017-03-02 18:31:14 +0000

    test code in comment should use the correct file name

--- Core/CholeskyUpdate/MImplementation/CholeskyUpdate.m
+++ Core/CholeskyUpdate/MImplementation/CholeskyUpdate.m
@@ -26,5 +26,5 @@ function [ R, p ] = CholeskyUpdate( R, x, SignString )
     p = 0;
 end
 
-% while true; R = randn( 10 ); A = R' * R; R = chol( A ); x = randn( 10, 1 ); [ R1, p ] = cholupdate( R, x, '+' ); if p == 0; disp( max( max( abs( R1' * R1 - A - x * x' ) ) ) ); end; end;
-% while true; R = randn( 10 ); A = R' * R; R = chol( A ); x = randn( 10, 1 ); [ R1, p ] = cholupdate( R, x, '-' ); if p == 0; disp( max( max( abs( R1' * R1 - A + x * x' ) ) ) ); end; end;
+% while true; R = randn( 10 ); A = R' * R; R = chol( A ); x = randn( 10, 1 ); [ R1, p ] = CholeskyUpdate( R, x, '+' ); if p == 0; disp( max( max( abs( R1' * R1 - A - x * x' ) ) ) ); end; end;
+% while true; R = randn( 10 ); A = R' * R; R = chol( A ); x = randn( 10, 1 ); [ R1, p ] = CholeskyUpdate( R, x, '-' ); if p == 0; disp( max( max( abs( R1' * R1 - A + x * x' ) ) ) ); end; end;
