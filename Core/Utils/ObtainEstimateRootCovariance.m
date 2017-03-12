c8795d4756a4f70beb5167ad0b3158783a098dd7
Tom Holden <thomas.holden@gmail.com>
2017-03-12 19:14:42 +0200

    error messages plus extra asserts in ObtainEstimateRootCovariance

--- Core/Utils/ObtainEstimateRootCovariance.m
+++ Core/Utils/ObtainEstimateRootCovariance.m
@@ -1,6 +1,6 @@
 function [ RootCovariance, InvRootCovariance, LogDetCovariance ] = ObtainEstimateRootCovariance( Covariance, StdDevThreshold )
 
-    assert( all( isfinite( Covariance(:) ) ), 'ESTNLSS:ObtainEstimateRootCovariance:NonFiniteInputCovariance' );
+    assert( all( isfinite( Covariance(:) ) ), 'ESTNLSS:ObtainEstimateRootCovariance:NonFiniteInputCovariance', 'ObtainEstimateRootCovariance was called on a non-finite input covariance.' );
 
     Covariance = full( 0.5 * ( Covariance + Covariance' ) );
     [ U, D ] = schur( Covariance, 'complex' );
@@ -9,11 +9,23 @@ function [ RootCovariance, InvRootCovariance, LogDetCovariance ] = ObtainEstimat
     IDv = RootD > StdDevThreshold;
     Usub = real( U( :, IDv ) );
     RootCovariance = Usub * diag( RootD( IDv ) );
+    
+    assert( all( isfinite( RootCovariance(:) ) ), 'ESTNLSS:ObtainEstimateRootCovariance:NonFiniteOutputRootCovariance', 'ObtainEstimateRootCovariance returned a non-finite output root covariance.' );
+
     if nargout > 1
+
         InvRootCovariance = diag( 1 ./ RootD( IDv ) ) * Usub';
+        
+        assert( all( isfinite( InvRootCovariance(:) ) ), 'ESTNLSS:ObtainEstimateRootCovariance:NonFiniteOutputInvRootCovariance', 'ObtainEstimateRootCovariance returned a non-finite output inverse root covariance.' );
+        
         if nargout > 2
+
             LogDetCovariance = sum( log( RootD ) );
+
+            assert( isfinite( LogDetCovariance ), 'ESTNLSS:ObtainEstimateRootCovariance:NonFiniteLogDetCovariance', 'ObtainEstimateRootCovariance returned a non-finite output log det covariance.' );
+        
         end
+        
     end
     % InvRootCovariance * RootCovariance = diag( 1 ./ RootD( IDv ) ) * Usub' * Usub * diag( RootD( IDv ) ) = eye
     % RootCovariance * InvRootCovariance = Usub * diag( RootD( IDv ) ) * diag( 1 ./ RootD( IDv ) ) * Usub' = Usub * Usub'
