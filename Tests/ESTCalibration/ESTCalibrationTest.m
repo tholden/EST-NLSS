b1086091297927928338abae5c0ce1620db90a73
tholden <thomas.holden@gmail.com>
2017-03-19 15:34:26 +0000

    switched ESTCalibrationTest to use new function

--- Tests/ESTCalibration/ESTCalibrationTest.m
+++ Tests/ESTCalibration/ESTCalibrationTest.m
@@ -5,8 +5,8 @@ addpath ../../Core/StudentTDist
 addpath ../../Core/ESTDist
 addpath ../../Core/Utils
 
-Order = 4;
-N = 10;
+N = 1;
+FilterCubatureDegree = 51;
 
 xi = 10 * randn( N, 1 );
 RootOmega = 0.1 * randn( N, N );
@@ -19,65 +19,7 @@ nu = 4.5 + 4 * randn ^ 2; % Inf;
 disp( 'tau, nu:' );
 disp( [ tau, nu ] );
 
-IntDim = N + 2;
-
-log_tcdf_tau_nu = StudentTLogCDF( tau, nu );
-
-if log_tcdf_tau_nu == 0
-    IntDim = IntDim - 1;
-    cholOmega = CholeskyUpdate( cholOmega, delta );
-    Omega = cholOmega' * cholOmega;
-    delta = zeros( N, 1 );
-end
-
-if isfinite( nu )
-    [ Weights, pTmp, T ] = fwtpts( IntDim, Order );
-    disp( 'T:' );
-    disp( T );
-    PhiN10 = normcdf( pTmp( end, : ) );
-    if log_tcdf_tau_nu < 0
-        N11Scaler = realsqrt( 0.5 * ( nu + 1 ) ./ gammaincinv( PhiN10, 0.5 * ( nu + 1 ), 'upper' ) );
-    else
-        N11Scaler = realsqrt( 0.5 * nu ./ gammaincinv( PhiN10, 0.5 * nu, 'upper' ) );
-    end
-end
-
-if ~isfinite( nu ) || all( abs( N11Scaler - 1 ) <= realsqrt( eps ) )
-    IntDim = IntDim - 1;
-    [ Weights, pTmp, T ] = fwtpts( IntDim, Order );
-    disp( 'T:' );
-    disp( T );
-else
-    pTmp( end, : ) = [];
-end
-
-if log_tcdf_tau_nu < 0
-    PhiN0 = normcdf( pTmp( end, : ) );
-    pTmp( end, : ) = [];
-    
-    ICDFTmp = 1 - ( 1 - PhiN0 ) * exp( log_tcdf_tau_nu );
-    FInvEST = zeros( size( ICDFTmp ) );
-    FInvESTSelectLeft = ICDFTmp <= 0.5;
-    FInvEST( FInvESTSelectLeft ) = StudentTInvLogCDF( reallog( ICDFTmp( FInvESTSelectLeft ) ), nu );
-    FInvEST( ~FInvESTSelectLeft ) = -StudentTInvLogCDF( log_tcdf_tau_nu + reallog( 1 - PhiN0( ~FInvESTSelectLeft ) ), nu );
-    
-    tpdfRatio = exp( StudentTLogPDF( tau, nu ) - log_tcdf_tau_nu );
-    
-    ICDFTmp = 1 - 0.5 * exp( log_tcdf_tau_nu );
-    if ICDFTmp <= 0.5
-        MedT = StudentTInvLogCDF( reallog( ICDFTmp ), nu );
-    else
-        MedT = -StudentTInvLogCDF( log_tcdf_tau_nu - 0.693147180559945, nu ); % log( 0.5 ) = -0.693147180559945
-    end
-    
-    N11Scaler = N11Scaler .* realsqrt( ( nu + FInvEST .^ 2 ) / ( 1 + nu ) );
-else
-    FInvEST = zeros( size( Weights ) );
-    tpdfRatio = 0;
-    MedT = 0;
-end
-
-ESTPoints = bsxfun( @plus, cholOmega' * bsxfun( @times, pTmp, N11Scaler ) + bsxfun( @times, delta, FInvEST ), xi );
+[ Weights, ESTPoints, NCubaturePoints ] = GetESTCubaturePoints( xi, Omega, delta, tau, nu, FilterCubatureDegree );
 
 mu = sum( bsxfun( @times, ESTPoints, Weights ), 2 );
 DemeanedESTPoints = bsxfun( @minus, ESTPoints, mu );
@@ -88,34 +30,12 @@ Sigma = DemeanedESTPoints * Weighted_DemeanedESTPoints';
 
 lambda = ESTPoints( :, 1 );
 
-tauTtau = tau * tau;
-OPtauTtauDnu = 1 + tauTtau / nu;
-if isfinite( nu )
-    nuOnuM1 = nu / ( nu - 1 );
-else
-    nuOnuM1 = 1;
-end
-if log_tcdf_tau_nu < 0
-    ET1 = nuOnuM1 * OPtauTtauDnu * tpdfRatio;
-else
-    ET1 = 0;
-end
-xiAlt = mu - delta * ET1;
-
-lambdaAlt = xi + delta * MedT;
-
-disp( 'xi, xiAlt:' );
-disp( [ xi, xiAlt ] );
-
-disp( 'lambda, lambdaAlt:' );
-disp( [ lambda, lambdaAlt ] );
-
 cholSigma_muMlambda = cholSigma * ( mu - lambda );
 
 Zcheck = ( ( mu - lambda )' * DemeanedESTPoints ) / realsqrt( cholSigma_muMlambda' * cholSigma_muMlambda );
 
 meanZcheck = Zcheck * Weights';
-meanZcheck2 = Zcheck.^2 * Weights';
+meanZcheck2 = realpow( Zcheck, 2 ) * Weights';
 
 disp( 'EZ, EZ^2:' );
 disp( [ meanZcheck, meanZcheck2 ] );
@@ -131,8 +51,8 @@ idx1Zcheck = find( fZcheck / max( fZcheck ) > 0.005, 1 );
 idx2Zcheck = find( fZcheck / max( fZcheck ) > 0.005, 1, 'last' );
 plot( xiZcheck( idx1Zcheck : idx2Zcheck ), fZcheck( idx1Zcheck : idx2Zcheck ) );
 
-sZ3 = Zcheck.^3 * Weights';
-sZ4 = max( 3, Zcheck.^4 * Weights' );
+sZ3 = realpow( Zcheck, 3 ) * Weights';
+sZ4 = max( 3, realpow( Zcheck, 4 ) * Weights' );
 
 disp( 'EZ^3, EZ^3:' );
 disp( [ sZ3, sZ4 ] );
