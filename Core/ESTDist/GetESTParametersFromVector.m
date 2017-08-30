4f7bbcd0977159772d61d96691d215d1193ac204
tholden <thomas.holden@gmail.com>
2017-08-30 20:02:54 +0100

    need chol omega, not its inverse

--- Core/ESTDist/GetESTParametersFromVector.m
+++ Core/ESTDist/GetESTParametersFromVector.m
@@ -1,18 +1,17 @@
-function [ xi, InvCholOmega, delta, tau, nu, LogDetInvCholOmega ] = GetESTParametersFromVector( p, n )
+function [ xi, CholOmega, delta, tau, nu ] = GetESTParametersFromVector( p, n )
 
     tau = p( 1 );
     nu = exp( p( 2 ) );
     xi = p( 3 : ( n + 2 ) );
     delta = p( ( n + 3 ) : ( 2 * n + 2 ) );
-    LogDiagInvCholOmega = p( ( 2 * n + 3 ) : ( 3 * n + 2 ) );
-    LTInvCholOmega = p( ( 3 * n + 3 ) : ( 3 * n + 2 + 0.5 * n * ( n - 1 ) ) );
+    LogDiagCholOmega = p( ( 2 * n + 3 ) : ( 3 * n + 2 ) );
+    UpperCholOmega = p( ( 3 * n + 3 ) : ( 3 * n + 2 + 0.5 * n * ( n - 1 ) ) );
 
-    LogDetInvCholOmega = sum( LogDiagInvCholOmega );
-    DiagInvCholOmega = exp( LogDiagInvCholOmega );
+    DiagCholOmega = exp( LogDiagCholOmega );
     
-    InvCholOmega = tril( ones( n ), - 1 );
-    InvCholOmega( ~~InvCholOmega ) = LTInvCholOmega;
-    InvCholOmega = InvCholOmega + diag( DiagInvCholOmega );
+    CholOmega = triu( ones( n ), 1 );
+    CholOmega( ~~CholOmega ) = UpperCholOmega;
+    CholOmega = CholOmega + diag( DiagCholOmega );
 
 end
 
