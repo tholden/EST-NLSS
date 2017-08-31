e74d4fd656386cc907d039d1711e537b0435d7a1
tholden <thomas.holden@gmail.com>
2017-08-31 09:50:54 +0100

    add inverse paramter mapping

---
+++ Core/ESTDist/InvGetESTParametersFromVector.m
@@ -0,0 +1,17 @@
+function p = InvGetESTParametersFromVector( xi, CholOmega, delta, tau, nu )
+
+    n = length( xi );
+    p = zeros( 3 * n + 2 + 0.5 * n * ( n - 1 ), 1 );
+    
+    LogDiagCholOmega = reallog( diag( CholOmega ) );
+    UpperTriangle = triu( ones( n ), 1 );
+    UpperCholOmega = CholOmega( UpperTriangle == 1 );
+    
+    p( 1 ) = tau;
+    p( 2 ) = log( nu );
+    p( 3 : ( n + 2 ) ) = xi;
+    p( ( n + 3 ) : ( 2 * n + 2 ) ) = delta;
+    p( ( 2 * n + 3 ) : ( 3 * n + 2 ) ) = LogDiagCholOmega;
+    p( ( 3 * n + 3 ) : ( 3 * n + 2 + 0.5 * n * ( n - 1 ) ) ) = UpperCholOmega;
+
+end
