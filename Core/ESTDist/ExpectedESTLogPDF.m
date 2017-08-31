62f2c42b04ec3a2a6e85b15dc91fa67971b86fb3
tholden <thomas.holden@gmail.com>
2017-08-31 09:35:22 +0100

    complete function

--- Core/ESTDist/ExpectedESTLogPDF.m
+++ Core/ESTDist/ExpectedESTLogPDF.m
@@ -4,5 +4,22 @@ function [ log_y, Dlog_y ] = ExpectedESTLogPDF( p, X, W )
     
     [ xi, CholOmega, delta, tau, nu ] = GetESTParametersFromVector( p, n );
     
+    log_y_Points = ESTLogPDF( X, xi, CholOmega, delta, tau, nu, true );
+    
+    log_y = log_y_Points * W(:);
+    
+    if nargout > 1
+        k = length( p );
+        Dlog_y = zeros( 1, k );
+        p = complex( p );
+        se = sqrt( eps );
+        sei = se * 1i;
+        for i = 1 : k
+            pTmp = p( i );
+            p( i ) = pTmp + sei;
+            Dlog_y( i ) = imag( ExpectedESTLogPDF( p, X, W ) ) / se;
+            p( i ) = pTmp;
+        end
+    end
     
 end
