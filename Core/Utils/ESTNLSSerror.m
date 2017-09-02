70a8c964870d46365afd3951e06fa7b76a143898
tholden <thomas.holden@gmail.com>
2017-09-02 10:50:48 +0100

    set default behaviour under matlab to throw an error

--- Core/Utils/ESTNLSSerror.m
+++ Core/Utils/ESTNLSSerror.m
@@ -1,9 +1,10 @@
 function ESTNLSSerror( msgID, msg, varargin )
     if coder.target( 'MATLAB' )
-        warning( msgID, msg, varargin{:} );
-    end
-    % else
-        fprintf( 'Warning: %s\n', msgID );
+        % warning( msgID, msg, varargin{:} );
+        error( msgID, msg, varargin{:} );
+        % keyboard;
+    else
+        fprintf( 'Error: %s\n', msgID );
         fprintf( [ msg '\n' ], varargin{:} );
-    % end
+    end
 end
