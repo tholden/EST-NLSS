3aa497505a825f64b2d0d9fa3688f092078c497d
tholden <thomas.holden@gmail.com>
2017-09-02 16:40:45 +0100

    further complex coder support for realcholeskyupdate

--- Core/Utils/RealCholeskyUpdate.m
+++ Core/Utils/RealCholeskyUpdate.m
@@ -1,17 +1,30 @@
-function [ R, p ] = RealCholeskyUpdate( R, x, SignString )
+function [ R, p ] = RealCholeskyUpdate( Rin, xin, SignString )
     if nargin < 3 || strcmp( SignString, '+' )
         Sign = +1;
     else
         Sign = -1;
     end
     % derived from https://en.wikipedia.org/wiki/Cholesky_decomposition#Rank-one_update
-    x = x(:);
-    n = length( x );
+    xin = xin(:);
+    n = length( xin );
+    
+    if isreal( xin ) || ( ~isreal( Rin ) )
+        R = Rin;
+    else
+        R = complex( Rin );
+    end
+    
+    if isreal( Rin ) || ( ~isreal( xin ) )
+        x = xin;
+    else
+        x = complex( xin );
+    end
+    
     for k = 1 : n
         Rkk = R( k, k );
         xk = x( k );
         r2 = Rkk .* Rkk + Sign * xk .* xk;
-        if real( r2 ) <= 0 || R( k, k ) == 0
+        if real( r2 ) <= 0 || Rkk == 0
             if nargout < 2
                 ESTNLSSerror( 'ESTNLSS:NonPDFollowingRealCholeskyUpdate', 'RealCholeskyUpdate produced a non-positive-definite matrix.' );
             else
