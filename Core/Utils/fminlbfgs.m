e71155d459b25b50a5c451b33d682a381dbc693b
tholden <thomas.holden@gmail.com>
2017-09-02 11:31:30 +0100

    better scaling for finite difference derivatives

--- Core/Utils/fminlbfgs.m
+++ Core/Utils/fminlbfgs.m
@@ -749,13 +749,15 @@ function [data,fval,grad]=gradient_function(x,funfcn, data, optim)
             gstep=data.initialStepLength/1e6; 
             if(gstep>optim.DiffMaxChange), gstep=optim.DiffMaxChange; end
             if(gstep<optim.DiffMinChange), gstep=optim.DiffMinChange; end
+            mag_x = max( 1, abs( x ) );
             for i=1:length(x)
+                current_gstep = gstep * mag_x( i );
                 x_temp=x; x_temp(i)=x_temp(i)+gstep;
                 [fval_g]=funfcn((x_temp));
                 fval_g = fval_g( 1 );
                 data.funcCount=data.funcCount+1;
                 data.timeExtern=data.timeExtern;
-                grad(i)=(fval_g-fval)/gstep;
+                grad(i)=(fval_g-fval)/current_gstep;
             end
         end
         grad=grad(:);
